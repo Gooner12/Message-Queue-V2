ef262edc0760eb01927860843a3e7a1c0758ffdb
Gooner12 <kcbiaksh@gmail.com>
2022-10-30 21:43:24 +1100

    Added the change for the blog post

--- src/controllers/email-controller.ts
+++ src/controllers/email-controller.ts
@@ -2,7 +2,9 @@ import { Request, Response } from "express";
 import { addJob } from "../producer/job-producer";
 
 export const sendEmail = async (req: Request, res: Response) => {
+  // const workerArray = new Array(5);
+  // Array.from(Array(5)).forEach(() => console.log("1"));
   const { msg, ...otherInfo } = req.body;
-  await addJob({...otherInfo, html: `<p>${msg}</p>`});
+  await addJob({ ...otherInfo, html: `<p>${msg}</p>` });
   res.status(200).json("Ok");
 };
