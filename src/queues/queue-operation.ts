b46c722293f5972ae71365461c2125070ac7a9cc
Gooner12 <kcbiaksh@gmail.com>
2022-10-30 21:45:43 +1100

    Brought back to the original version

--- src/queues/queue-operation.ts
+++ src/queues/queue-operation.ts
@@ -11,62 +11,35 @@ type WorkNode = {
 export const emailQueue = new Queue(queueName, { connection });
 const queueEvents = new QueueEvents(queueName, { connection });
 
-const workerArray = Array.from(Array(4));
-for (let index in workerArray) {
-  workerArray[index] = new Worker(
-    queueName,
-    `${__dirname}/../worker/email-worker.ts`,
-    {
-      connection,
-      concurrency: 2,
-    }
+const emailWorkerFirst = new Worker(
+  queueName,
+  `${__dirname}/../worker/email-worker.ts`,
+  {
+    connection,
+  }
+);
 
-  );
-}
-
-console.log(`The number of worker: ${workerArray.length}`);
-let counter = 1;
-queueEvents.on("completed", ({ jobId: string, returnvalue: any }) => {
-  console.log(`Task completed : ${counter}`);
-  counter += 1;
-});
-
-// const emailWorkerFirst = new Worker(
-//   queueName,
-//   `${__dirname}/../worker/email-worker.ts`,
-//   {
-//     connection,
-//     // concurrency: 3,
-//   }
-// );
-
-// const emailWorkerSecond = new Worker(
-//   queueName,
-//   `${__dirname}/../worker/email-worker.ts`,
-//   {
-//     connection,
-//     // concurrency: 2,
-//   }
-// );
-
-
-
-
-
-// console.log(workerArray);
+const emailWorkerSecond = new Worker(
+  queueName,
+  `${__dirname}/../worker/email-worker.ts`,
+  {
+    connection,
+    concurrency: 3,
+  }
+);
 
 // checking which worker did the job
 const eventListener = (workerObject: WorkNode) => {
   workerObject.worker.on("completed", async (job: Job, result: any) => {
     console.log(`Completed by ${workerObject.name} worker.`);
-    // if (workerObject.name === "first") {
-    //   await workerObject.worker.close();
-    // }
+    if (workerObject.name === "first") {
+      await workerObject.worker.close();
+    }
   });
 };
 
-// eventListener({worker: emailWorkerFirst, name: "first"});
-// eventListener({worker: emailWorkerSecond, name: "second"});
+eventListener({worker: emailWorkerFirst, name: "first"});
+eventListener({worker: emailWorkerSecond, name: "second"});
 
 // handling the error from the events to prevent node from raising an unhandled exception and exiting the process.
 //  Otherwise, the worker may stop processing remaining jobs when error is emitted
@@ -75,11 +48,11 @@ queueEvents.on("error", (error) => {
 });
 
 // reporting progress
-// queueEvents.on("progress", (args: {jobId: string, data: number | object}) => {
-//   if (args.data == 50) {
-//     console.log("Finalising the email sending process");
-//   }
-//   if (args.data == 100) {
-//     console.log("Completed the send process");
-//   }
-// });
+queueEvents.on("progress", (args: {jobId: string, data: number | object}) => {
+  if (args.data == 50) {
+    console.log("Finalising the email sending process");
+  }
+  if (args.data == 100) {
+    console.log("Completed the send process");
+  }
+});
